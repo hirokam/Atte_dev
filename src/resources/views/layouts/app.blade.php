5097dd60a37b111a0dd2c7b78c953b0602ab8c04
hirokam <h.kameyama0329@gmail.com>
2023-12-20 00:22:37 +0900

    first commit

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="x-ua-compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atte</title>
+    <link rel="stylesheet" href="css/sanitize.css">
+    <link rel="stylesheet" href="css/common.css">
+    @yield('css')
+
+    <style>
+        body {
+            height: 100vh;
+        }
+    </style>
+</head>
+<body>
+    <header class="header">
+        <div class="header-content__left">
+            <a href="" class="header__title">
+                Atte
+            </a>
+        </div>
+        @yield('header')
+    </header>
+
+    <main class="main">
+        @yield('main')
+    </main>
+
+    <footer class="footer">
+        <small class="copyright">Atte,inc.</small>
+    </footer>
+</body>
+</html>
