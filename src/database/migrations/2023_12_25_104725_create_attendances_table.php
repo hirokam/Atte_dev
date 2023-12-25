444bfaa184361f16c86f465c654bd717eca588d5
hirokam <h.kameyama0329@gmail.com>
2023-12-25 22:17:17 +0900

    database

---
+++ src/database/migrations/2023_12_25_104725_create_attendances_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttendancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->id();
+            $table->date('workday');
+            $table->dateTime('work_start_time', $precision = 0);
+            $table->dateTime('work_end_time', $precision = 0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendances');
+    }
+}
